ec1eedba175c1ca6814100050571be17e468576c
appleRui <apple31581@gmail.com>
2022-01-10 09:26:53 +0900

    Init/220110/Setting TS And Vuetify

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-main>
+      <Header />
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+</style>
