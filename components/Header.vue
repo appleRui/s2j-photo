090cc64cb00750e8b7d81bbbc22896f96804ef40
appleRui <apple31581@gmail.com>
2022-01-11 00:11:27 +0900

    Fix/220111/Header Title And Icon

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,8 @@
 <template>
-  <v-toolbar dense fixed flat=true class="header">
+  <v-toolbar dense fixed flat="true" class="header">
     <div class="header__content">
-      <v-avatar size="20" tile>
-        <img src="@/assets/logo.png" alt="Logo" />
-      </v-avatar>
-      <span class="header__content_ttl">TDU 二部写真部</span>
+      <v-avatar size="20" tile> 📸 </v-avatar>
+      <span class="header__content_ttl">Tokyo Denki University s2j-photo</span>
     </div>
   </v-toolbar>
 </template>
@@ -18,11 +16,12 @@ export default Vue.extend({});
 <style lang="scss" scoped>
 .header {
   width: 100%;
-  position: fixed;
+  position: sticky;
   top: 0;
   left: 0;
   &__content {
     &_ttl {
+      font-size: 14px;
       margin-left: 0.25rem;
     }
   }
